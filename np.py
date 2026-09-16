67781900df76089fcbb7562e9dc2684625459d86
AhmadFazel7861 <ahmadfazelpaknehad071@gmail.com>
2026-09-16 09:11:47 +0430

    first commit

---
+++ np.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,5,6])
+
+print(arr)
+print(type(arr))
+print(np.__version__)
+
+arr1 = np.array((7,8,9,10))
+print(type(arr1))
+
+D0 = np.array(30)
+print(D0)
+D1 = np.array([1,2,3,4,5,6])
+print(D1)
+D2 = np.array([1,2]+[3,4])
+print(D2)
